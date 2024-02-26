b3f22030833819869b26b2871e76d3d12dfe4077
Cessnus <143093093+backDoora@users.noreply.github.com>
2024-02-27 00:08:08 +0500

    Add files via upload

---
+++ src/core/crvlpy/menu/modules/useful_menu.py
@@ -0,0 +1,18 @@
+def usr_choose_useful_act():
+        print("Выберите инструмент: ")
+        useful()
+
+def usr_choose_useful():
+    user_choice_var = usr_choose_useful_act()
+    if user_choice_var == 1:
+        pass
+    if user_choice_var == 2:
+        pass
+    if user_choice_var == 3:
+        pass
+
+def useful():
+    print("1) Случайные данные\n")
+    print("2) В разработке\n")
+    print("3) В разработке\n")
+    usr_choose_useful()
