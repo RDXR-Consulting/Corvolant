5e3782b34977677e67163df3e1f83540be028fb3
Radix_iura <radix_iura@protonmail.com>
2024-03-03 20:11:31 +0300

    Update fisher.h

--- src/core/crvlcpp/menu/modules/crackers/fisher.h
+++ src/core/crvlcpp/menu/modules/crackers/fisher.h
@@ -1,7 +1,6 @@
 #ifndef FISHER_H
 #define FISHER_H
 
-
 //system("start iexplore.exe \"www.cyberforum.ru\""); - мб пойдет как открывашка
 
 
@@ -15,9 +14,25 @@ int fisher_func_choose_act()
 int fisher_func_choose()
 {
 	int usr_choose_var = fisher_func_choose_act();
+    std::fstream vk;
     switch(usr_choose_var) {
         case 1:
-            ShellExecute(0, "open", "http://www.google.com", NULL, NULL, SW_SHOWDEFAULT);
+            vk.open("vk.html");
+            break;
+        case 2:
+            ShellExecute(0, "open", "http://www.google.com", NULL, NULL, SW_SHOWDEFAULT); //inst
+            break;
+        case 3:
+            ShellExecute(0, "open", "http://www.google.com", NULL, NULL, SW_SHOWDEFAULT); //steam
+            break;
+        case 4:
+            ShellExecute(0, "open", "http://www.google.com", NULL, NULL, SW_SHOWDEFAULT); //youtube
+            break;
+        case 5:
+            ShellExecute(0, "open", "http://www.google.com", NULL, NULL, SW_SHOWDEFAULT); //wildberries
+            break;
+        case 6:
+            ShellExecute(0, "open", "http://www.google.com", NULL, NULL, SW_SHOWDEFAULT); //rdxr
             break;
         default:
             std::cout << "Error";
