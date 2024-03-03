5d83160a67d26e0c3fa1c8ad99b06e7215d815e0
Radix_iura <radix_iura@protonmail.com>
2024-03-03 19:29:56 +0300

    Create fisher.h

---
+++ src/core/crvlcpp/menu/modules/crackers/fisher.h
@@ -0,0 +1,55 @@
+#ifndef FISHER_H
+#define FISHER_H
+
+#include <iostream>
+
+
+int fisher_func_choose_act()
+{
+	cout << "Выберите сайт для фишинга:";
+	int fisher_func_choose_act; cin >> fisher_func_choose_act; cin.ignore(32767, '\n');
+	return fisher_func_choose_act; 
+}
+
+
+int fisher_func_choose()
+{
+	int usr_choose_var = fisher
+}
+
+
+
+
+
+
+int usr_choose_crackers()
+{
+    int user_choose_var = usr_choose_crackers_act();
+    switch(user_choose_var)
+    {
+        case 1:
+            bruteforce_func();
+            break;
+        default:
+            cout << "Error";
+    }
+    return 0;
+}
+
+
+void crackers()
+{
+    cout << "Брутфорс - 1\n";
+    cout << "В разработке - 2\n";
+    cout << "В разработке - 3\n";
+    usr_choose_crackers();
+}
+
+
+
+system("start iexplore.exe \"www.cyberforum.ru\"");
+
+
+
+}
+#endif
