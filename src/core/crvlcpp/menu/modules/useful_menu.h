02effc9682d45801407cfa3e71346fcba1de8fcd
Cessnus <143093093+backDoora@users.noreply.github.com>
2024-03-07 00:33:32 +0500

    useful_menu.h

--- src/core/crvlcpp/menu/modules/useful_menu.h
+++ src/core/crvlcpp/menu/modules/useful_menu.h
@@ -3,38 +3,37 @@
 
 #include "useful/randata.h"
 
-using namespace std;
 
 int usr_choose_useful_act()
 {
-    cout << "Выберите инструмент: ";
-    int usr_choose_useful_var; cin >> usr_choose_useful_var; cin.ignore(32767, '\n');
+    std::cout << "Выберите инструмент: ";
+    int usr_choose_useful_var; std::cin >> usr_choose_useful_var; cin.ignore(32767, '\n');
     return usr_choose_useful_var;
 }
 
 int usr_choose_useful()
 {
     int user_choose_var = usr_choose_useful_act();
-    switch(user_choose_var)
+    if(user_choose_var == 1)
     {
-        case 1:
-            randata_func();
-            break;
-        default:
-            cout << "Error";
+        randata_func();
     }
+    else
+    {
+        std::cout << "Error";
+    }
+
     return 0;
 }
 
 
-void useful()
-{
-  cout << "Случайные данные - 1\n";
-  cout << "В разработке - 2\n";
-  cout << "В разработке - 3\n";
+void useful() 
+{             
+    std::cout << "Случайные данные - 1\n";
+    std::cout << "В разработке - 2\n";
+    std::cout << "В разработке - 3\n";
   usr_choose_useful();
 }
 
 
 #endif
-
