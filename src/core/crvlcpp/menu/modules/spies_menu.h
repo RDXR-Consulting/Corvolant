917672ca37d23ed8b2d60e09a121c0e74079accb
Cessnus <143093093+backDoora@users.noreply.github.com>
2024-03-07 00:21:44 +0500

    spies_menu.h

--- src/core/crvlcpp/menu/modules/spies_menu.h
+++ src/core/crvlcpp/menu/modules/spies_menu.h
@@ -3,35 +3,28 @@
 
 #include "spies/keylogger.h"
 
-using namespace std;
 
 int usr_choose_spies_act()
 {
-    cout << "Выберите инструмент: ";
-    int usr_choose_spies_var; cin >> usr_choose_spies_var; cin.ignore(32767, '\n');
+    std::cout << "Выберите инструмент: ";
+    int usr_choose_spies_var;  std::cin >> usr_choose_spies_var; cin.ignore(32767, '\n');
     return usr_choose_spies_var;
 }
 
 int usr_choose_spies()
 {
-    int user_choose_var = usr_choose_spies_act();
-    switch(user_choose_var)
-    {
-        case 1:
-            keylogger_func();
-            break;
-        default:
-            cout << "Error";
-    }
+    int usr_choose_var = usr_choose_spies_act();
+    if(usr_choose_var == 1){keylogger_func();}
+    else{std::cout << "Error";}
     return 0;
 }
 
 
 void spies()
 {
-  cout << "Кейлоггер - 1\n";
-  cout << "В разработке - 2\n";
-  cout << "В разработке - 3\n";
+    std::cout << "Кейлоггер - 1\n";
+    std::cout << "В разработке - 2\n";
+    std::cout << "В разработке - 3\n";
   usr_choose_spies();
 }
 
