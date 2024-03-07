4398d4db5ccef255bff42c2af837058ada43c705
Cessnus <143093093+backDoora@users.noreply.github.com>
2024-03-07 21:17:27 +0500

    crvl.cpp

--- src/core/crvlcpp/crvl.cpp
+++ src/core/crvlcpp/crvl.cpp
@@ -1,20 +1,60 @@
-// Стандартные библиотеки
-#include <iostream>
-#include <windows.h>
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
 
-// Пользовательские библиотеки
-#include "menu/main_menu.h"
+#include "modules/crackers_menu.h"
+#include "modules/miners_menu.h"
+#include "modules/spies_menu.h"
+#include "modules/useful_menu.h"
 
-// Пространства имен
-using namespace std;
 
-int main()
+int usr_choose_menu_act()
 {
-  SetConsoleCP(65001);
-  SetConsoleOutputCP(65001);
-  // запуск ГИ
-  cout << "ПРИВЕТСТВИЕ\n";
-  // Запуск меню
-  menu();
+  std::cout << "Выберите раздел: ";
+  int usr_choose_menu_var; cin >> usr_choose_menu_var; cin.ignore(32767, '\n');
+  return usr_choose_menu_var;
+}
+
+int usr_choose_menu()
+{
+  int user_choose_var = usr_choose_menu_act();
+  if(user_choose_var ==1)
+  {
+      crackers();
+  }
+  if(user_choose_var ==2)
+  {
+      miners();
+  }
+  if(user_choose_var ==3)
+  {
+      spies();
+  }
+  if(user_choose_var ==4)
+  {
+      useful();
+  }
+  else
+  {
+      std::cout << "Error";
+  }
+    return 0;
+}
+
+
+int menu()
+{
+  //gui - запуск
+    cout <<"╭────────────────────────────────────────────╮\n";
+    cout <<"│          Г Л А В Н О Е    М Е Н Ю          │\n";
+    cout <<"│============================================│\n";
+    cout <<"│ 1) Взломщики                               │\n";
+    cout <<"│ 2) Майнеры                                 │\n";
+    cout <<"│ 3) Шпионы                                  │\n";
+    cout <<"│ 4) Полезности                              │\n";
+    cout <<"│                                            │\n";
+    cout <<"╰────────────────────────────────────────────╯\n";
+  usr_choose_menu();
   return 0;
 }
+
+#endif
