42e9c2f9678d3f44439f61cffdc97823c1376499
Radix_iura <radix_iura@protonmail.com>
2024-03-08 02:21:59 +0300

    Update crvl.py

--- src/core/crvlpy/crvl.py
+++ src/core/crvlpy/crvl.py
@@ -1,9 +1,24 @@
-#DunastyC 5.03.24#
+import sys
+from PyQt5 import QtWidgets
 from menu import main_menu
 
+import design
+
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+
+
 def main():
-    main_menu.menu()
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+    
 
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
