71fcabca76c6e21b80877aebe4eca7e36f3d643e
Cessnus <143093093+backDoora@users.noreply.github.com>
2024-03-24 14:16:06 +0500

    useful_menu.h

--- src/core/crvlcpp/menu/modules/useful_menu.h
+++ src/core/crvlcpp/menu/modules/useful_menu.h
@@ -2,13 +2,13 @@
 #define USEFUL_MENU_H
 
 #include "useful/randata.h"
-
+#include "useful/keygen.h"
 
 
 int usr_choose_useful_act()
 {
     std::cout << "Выберите инструмент: ";
-    int usr_choose_useful_var; std::cin >> usr_choose_useful_var; cin.ignore(32767, '\n');
+    int usr_choose_useful_var; std::cin >> usr_choose_useful_var; cin.ignore(32766, '\n');
     return usr_choose_useful_var;
 }
 
@@ -16,6 +16,10 @@ int usr_choose_useful()
 {
     int user_choose_var = usr_choose_useful_act();
     if(user_choose_var == 1)
+    {
+        keygen_func();
+    }
+    if(user_choose_var == 2)
     {
         randata_func();
     }
@@ -30,11 +34,11 @@ int usr_choose_useful()
 
 void useful()
 {
-    std::cout <<"\n╭────────────────────────────────────────────╮\n";
+std::cout <<"\n╭────────────────────────────────────────────╮\n";
     std::cout <<"│               П О Л Е З Н О Е              │\n";
     std::cout <<"│============================================│\n";
-    std::cout <<"│ 1) Случайные данные                        │\n";
-    std::cout <<"│ 2) В разработке                            │\n";
+    std::cout <<"│ 1) Генератор паролей                       │\n";
+    std::cout <<"│ 2) Случайные данные                        │\n";
     std::cout <<"│ 3) В разработке                            │\n";
     std::cout <<"│                                            │\n";
     std::cout <<"╰────────────────────────────────────────────╯\n";
