229bcb7d90e25e80f6dc1fdeb92f8d6913556807
DunastyCode <104996756+DunastyCode@users.noreply.github.com>
2024-04-09 23:11:04 +0500

    Add files via upload

--- src/core/crvlpy/crvl.py
+++ src/core/crvlpy/crvl.py
@@ -1,11 +1,9 @@
 #DunastyC 5.03.24#
 #from art import tprint
 from menu import main_menu
-import sys  # sys нужен для передачи argv в QApplication
-from PyQt5 import QtWidgets
-import design
 
 def main():
+    #print('Started')
     main_menu.menu()
 
 if __name__ == "__main__":
