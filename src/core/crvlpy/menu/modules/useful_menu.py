371876457025da30d6d4eb7183727ff1af6fbb93
Radix_iura <radix_iura@protonmail.com>
2024-04-15 19:19:25 +0300

    Update useful_menu.py

--- src/core/crvlpy/menu/modules/useful_menu.py
+++ src/core/crvlpy/menu/modules/useful_menu.py
@@ -1,7 +1,9 @@
 from menu.modules.useful import rundata as run
 
+
 def usr_choose_useful_act():
     return int(input("Выберите инструмент: "))
+
 
 def usr_choose_useful():
     user_choice_var = usr_choose_useful_act()
@@ -12,12 +14,15 @@ def usr_choose_useful():
     if user_choice_var == 3:
         pass
 
+
 def useful():
     print("╭────────────────────────────────────────────╮")
     print("│            П О Л Е З Н О С Т И             │")
     print("│============================================│")
-    print("│ 1) Случайные данные                        │")
-    print("│ 2) В разработке                            │")
+    print("│ 1) Случайные данные (имя, фамилия и тд.    │")
+    print("│ 2) Скрипты                                 │")
+    print("│ 3) Быстрый сбор данных о системе           │")
+    print("│ 4) КОРОВА                                  │")
     print("│                                            │")
     print("╰────────────────────────────────────────────╯\n")
     usr_choose_useful()
