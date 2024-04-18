482a3de4b0c7d506470e61a8212b39e1355670b0
Radix_iura <radix_iura@protonmail.com>
2024-04-18 14:16:10 +0300

    Update crackers_menu.py

--- src/core/crvlpy/menu/modules/crackers_menu.py
+++ src/core/crvlpy/menu/modules/crackers_menu.py
@@ -1,9 +1,12 @@
 from menu.modules.crackers import bruteforce as brut
 from menu.modules.crackers import fisher as fish
 
+from src.core.crvlpy.menu.main_menu import menu
+
 
 def usr_choose_crackers_act():
     return int(input("Выберите инструмент: "))
+
 
 def usr_choose_crackers():
     user_choose_var = usr_choose_crackers_act()
@@ -11,9 +14,17 @@ def usr_choose_crackers():
         brut.bruteforce_func()
     elif user_choose_var == 2:
         fish.fisher_func()
+    elif user_choose_var == 0:
+        menu()
     else:
         print("Error")
 
+
 def crackers():
-    print("╭────────────────────────────────────────────╮\n│             В З Л О М Щ И К И              │\n│============================================│\n│ 1) Брутфорс                                │\n│ 2) Фишер                                   │\n│ 3) Анализ сетей                            │\n│ 4) Ботнет (кукольный театр ддос + реверс)  │\n│ 5) Генератор помех (сети и реверс)         │\n│                                            │\n╰────────────────────────────────────────────╯\n")
+    print("╭────────────────────────────────────────────╮\n│             В З Л О М Щ И К И              "
+          "│\n│============================================│\n│ 1) Брутфорс                                │\n│ 2) "
+          "Фишер                                   │\n│ 3) Анализ сетей                            │\n│ 4) Ботнет ("
+          "кукольный театр ддос + реверс)  │\n│ 5) Генератор помех (сети и реверс)         │\n│ 0) Назад              "
+          "                     │\n│                                            "
+          "│\n╰────────────────────────────────────────────╯\n")
     usr_choose_crackers()
