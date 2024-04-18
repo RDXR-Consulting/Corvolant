d2138a4b73255ffdb1e7e5e84f25ea155eb7634a
Radix_iura <radix_iura@protonmail.com>
2024-04-18 14:49:56 +0300

    Update spies_menu.py

--- src/core/crvlpy/menu/modules/spies_menu.py
+++ src/core/crvlpy/menu/modules/spies_menu.py
@@ -1,17 +1,25 @@
-#DunastyC 5.03.24#
 from menu.modules.spies import keylogger as keylog
 
-def usr_choose_spies_act():
+
+def usr_choice_spies_act():
     return int(input("Выберите инструмент: "))
 
-def usr_choose_spies():
-    user_choose_var = usr_choose_spies_act()
-    if user_choose_var == 1:
+
+def usr_choice_spies():
+    user_choice_var = usr_choice_spies_act()
+    if user_choice_var == 1:
         keylog.keylogger_func()
+    elif user_choice_var == 0:
+        pass
     else:
         print('error')
         pass
 
+
 def spies():
-    print("╭────────────────────────────────────────────╮\n│                Ш П И О Н Ы                 │\n│============================================│\n│ 1) Кейлоггер                               │\n│ 2) Троян (черный ящик)                     │\n│ 3) Поисковая лаборатория(список осинтинстру│\n│                                            │\n╰────────────────────────────────────────────╯\n")
-    usr_choose_spies()
+    print(
+        "╭────────────────────────────────────────────╮\n│                Ш П И О Н Ы                 "
+        "│\n│============================================│\n│ 1) Кейлоггер                               │\n│ 2) "
+        "Троян (черный ящик)                     │\n│ 3) Поисковая лаборатория(список осинт)     │\n│ 0) Назад        "
+        "                           │\n╰────────────────────────────────────────────╯\n")
+    usr_choice_spies()
