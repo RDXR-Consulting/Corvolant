493f709533b5d7cb27d5db0653e955a3fa16355a
Radix_iura <radix_iura@protonmail.com>
2024-04-18 14:56:20 +0300

    Update crackers_menu.py

--- src/core/crvlpy/menu/modules/crackers_menu.py
+++ src/core/crvlpy/menu/modules/crackers_menu.py
@@ -4,27 +4,31 @@ from menu.modules.crackers import fisher as fish
 from src.core.crvlpy.menu.main_menu import menu
 
 
-def usr_choose_crackers_act():
+def usr_choice_crackers_act():
     return int(input("Выберите инструмент: "))
 
 
-def usr_choose_crackers():
-    user_choose_var = usr_choose_crackers_act()
-    if user_choose_var == 1:
+def usr_choice_crackers():
+    user_choice_var = usr_choice_crackers_act()
+    if user_choice_var == 1:
         brut.bruteforce_func()
-    elif user_choose_var == 2:
+    elif user_choice_var == 2:
         fish.fisher_func()
-    elif user_choose_var == 0:
+    elif user_choice_var == 0:
         menu()
     else:
         print("Error")
 
 
 def crackers():
-    print("╭────────────────────────────────────────────╮\n│             В З Л О М Щ И К И              "
-          "│\n│============================================│\n│ 1) Брутфорс                                │\n│ 2) "
-          "Фишер                                   │\n│ 3) Анализ сетей                            │\n│ 4) Ботнет ("
-          "кукольный театр ддос + реверс)  │\n│ 5) Генератор помех (сети и реверс)         │\n│ 0) Назад              "
-          "                     │\n│                                            "
-          "│\n╰────────────────────────────────────────────╯\n")
-    usr_choose_crackers()
+    print("╭────────────────────────────────────────────╮\n"
+          "│          Г Л А В Н О Е    М Е Н Ю          │\n"
+          "│============================================│\n"
+          "│ 1) Брутфорс                                │\n"
+          "│ 2) Фишер                                   │\n"
+          "│ 3) Анализ сетей                            │\n"
+          "│ 4) Ботнет                                  │\n"
+          "│ 5) Генератор помех                         │\n"
+          "│ 0) Назад                                   │\n"
+          "╰────────────────────────────────────────────╯\n")
+    usr_choice_crackers()
