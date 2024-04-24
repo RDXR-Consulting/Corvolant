0cebf05d71189f9eca378a169ab47d0a008dcf48
Radix_iura <radix_iura@protonmail.com>
2024-04-24 21:20:15 +0300

    Update randata.h

--- src/core/crvlcpp/menu/modules/useful/randata.h
+++ src/core/crvlcpp/menu/modules/useful/randata.h
@@ -1,10 +1,44 @@
 #ifndef RANDATA_H
 #define RANDATA_H
 
-int randata_func()
+
+int usr_choose_country_act()
 {
-    std::cout << "Здесь будут случайные данные\n";
+    std::cout << "Укажите государство/национальность: ";
+    int usr_choose_country_var; cin >> usr_choose_country_var; cin.ignore(32767, '\n');
+    return usr_choose_country_var;
 }
 
+int usr_choose_age_act()
+{
+    std::cout << "Укажите возраст: ";
+    int usr_choose_country_var; cin >> usr_choose_country_var; cin.ignore(32767, '\n');
+    return usr_choose_country_var;
+}
+
+int usr_choose_occupation_act()
+{
+    std::cout << "Укажите возраст: ";
+    int usr_choose_occupation_var; cin >> usr_choose_occupation_var; cin.ignore(32767, '\n');
+    return usr_choose_occupation_var;
+}
+
+
+
+int randata_func()
+{
+    cout << "Россия - 1\n"
+            "Америка - 2\n"
+            "Китай - 3\n"
+            "Европа - 4\n"
+    int user_choose_country_var = usr_choose_country_act();
+
+    cout << "(15 - 25 лет) - 1\n"
+            "(25 - 35 лет) - 2\n"
+            "(35 - 45 лет) - 3\n"
+            "(45 - 55 лет) - 4\n"
+    int user_choose_age_var = usr_choose_age_act();
+
+}
 
 #endif
