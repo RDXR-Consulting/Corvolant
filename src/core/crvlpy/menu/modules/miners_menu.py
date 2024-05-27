45c0e1029bf7396f4150a4f7a6e7da81d6428c15
Radix_iura <radix_iura@protonmail.com>
2024-05-27 15:20:54 +0300

    Update miners_menu.py

--- src/core/crvlpy/menu/modules/miners_menu.py
+++ src/core/crvlpy/menu/modules/miners_menu.py
@@ -1,25 +1,31 @@
-#from menu.modules.miners import 
+from menu.modules.miners import autominer as mnr
+from menu.modules.miners import transport as trp
+from menu.modules.miners import asicminer as amr
 
 
-def usr_choose_miners_act():
-    return int(input("Выберите инструмент: "))
-
-def usr_choose_miners():
-    user_choice_var = usr_choose_miners_act()
-    if user_choice_var == 1:
-        pass
-    if user_choice_var == 2:
-        pass
-    if user_choice_var == 3:
-        pass
-
-def miners():
+def minermenu():
     print("╭────────────────────────────────────────────╮\n"
           "│              М А Й Н Е Р Ы                 │\n"
           "│============================================│\n"
           "│ 1) Автомайнер (вкл по нажатию автоматом    │\n"
-          "│ 2) Переносчик                              │\n"
+          "│ 2) Переносчик (транспорт)                  │\n"
           "│ 3) ASIC - майнинг                          │\n"
+          "│ 0) Назад                                   │\n"
           "╰────────────────────────────────────────────╯\n")
-    usr_choose_miners()
-    
+
+    choosing = True
+    while choosing:
+        usr_choose = int(input("Выберите раздел: "))
+        if usr_choose == 1:
+            mnr.autominer_func()
+            choosing = False
+        elif usr_choose == 2:
+            trp.transport_func()
+            choosing = False
+        elif usr_choose == 3:
+            amr.asicminer_func()
+            choosing = False
+        elif usr_choose == 0:
+            exit()
+        else:
+            print("Вы ввели что то неправильное")
