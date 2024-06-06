a8581000a960557593e2e13013a5de39471ac6bd
Cessnus <143093093+backDoora@users.noreply.github.com>
2024-06-06 21:09:44 +0500

    Update spies_menu.py

--- src/core/crvlpy/menu/modules/spies_menu.py
+++ src/core/crvlpy/menu/modules/spies_menu.py
@@ -3,35 +3,30 @@ from menu.modules.spies import keylogger as keylog
 from menu.modules.spies import blackBox as box
 from menu.modules.spies import osint
 
-
-def usr_choice_spies_act():
-    return int(input("Выберите инструмент: "))
-
-
 def spiesmenu():
-    print(
-        "╭────────────────────────────────────────────╮\n"
-        "│                Ш П И О Н Ы                 │\n"
-        "│============================================│\n"
-        "│ 1) Кейлоггер                               │\n"
-        "│ 2) Троян (черный ящик)                     │\n"
-        "│ 3) Поисковая лаборатория(список осинт)     │\n"
-        "│ 0) Назад                                   │\n"
-        "╰────────────────────────────────────────────╯\n")
+    print("╭────────────────────────────────────────────╮\n"
+          "│                Ш П И О Н Ы                 │\n"
+          "│============================================│\n"
+          "│ 1) Кейлоггер                               │\n"
+          "│ 2) Троян (черный ящик)                     │\n"
+          "│ 3) Поисковая лаборатория(список осинт)     │\n"
+          "│ 0) Назад                                   │\n"
+          "╰────────────────────────────────────────────╯\n")
 
     choosing = True
     while choosing:
         usr_choose = int(input("Выберите раздел: "))
-    if usr_choose == 1:
-        keylog.keylogger_func()
-        choosing = False
-    elif usr_choose == 2:
-        box.blackBox_func
-        choosing = False
-    elif usr_choose == 3:
-        osint.osint_func()
-        choosing = False
-    elif usr_choose == 0:
-        main_menu.glavmenu()
-    else:
-        print("Вы ввели что-то неправильное")
+        if usr_choose == 1:
+            keylog.keylogger_func()
+            choosing = False
+        elif usr_choose == 2:
+            box.blackBox_func()
+            choosing = False
+        elif usr_choose == 3:
+            osint.osint_func()
+            choosing = False
+        elif usr_choose == 0:
+            main_menu.glavmenu()
+            choosing = False
+        else:
+            print("Вы ввели что-то неправильное")
