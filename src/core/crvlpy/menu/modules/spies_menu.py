a4be08cb2a2e95a81561a4bec7d7f2dee24f5423
Cessnus <143093093+backDoora@users.noreply.github.com>
2024-06-06 20:01:36 +0500

    Update spies_menu.py

--- src/core/crvlpy/menu/modules/spies_menu.py
+++ src/core/crvlpy/menu/modules/spies_menu.py
@@ -1,22 +1,13 @@
 from menu.modules.spies import keylogger as keylog
+from menu.modules.spies import blackBox as box
+from menu.modules.spies import osint
 
 
 def usr_choice_spies_act():
     return int(input("Выберите инструмент: "))
 
 
-def usr_choice_spies():
-    user_choice_var = usr_choice_spies_act()
-    if user_choice_var == 1:
-        keylog.keylogger_func()
-    elif user_choice_var == 0:
-        pass
-    else:
-        print('error')
-        pass
-
-
-def spies():
+def spiesmenu():
     print(
         "╭────────────────────────────────────────────╮\n"
         "│                Ш П И О Н Ы                 │\n"
@@ -24,6 +15,22 @@ def spies():
         "│ 1) Кейлоггер                               │\n"
         "│ 2) Троян (черный ящик)                     │\n"
         "│ 3) Поисковая лаборатория(список осинт)     │\n"
-        "│ 0) Назад                                   │\n"
+        "│ 0) Выйти                                   │\n"
         "╰────────────────────────────────────────────╯\n")
-    usr_choice_spies()
+
+    choosing = True
+    while choosing:
+        usr_choose = int(input("Выберите раздел: "))
+    if usr_choose == 1:
+        keylog.keylogger_func()
+        choosing = False
+    elif usr_choose == 2:
+        box.blackBox_func
+        choosing = False
+    elif usr_choose == 3:
+        osint.osint_func()
+        choosing = False
+    elif usr_choose == 0:
+        exit()
+    else:
+        print("Вы ввели что-то неправильное")
